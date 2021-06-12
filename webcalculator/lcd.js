bccdd87cd07135f83641e0835323b9b25d95e096
CodingSifu <38577527+CodingSifu@users.noreply.github.com>
2021-06-12 17:26:14 -0400

    Keyboard key table switched to sentences for better screen reader compatibility

--- webcalculator/lcd.js
+++ webcalculator/lcd.js
@@ -1,7 +1,6 @@
 let lcd={
     display: document.querySelector('#LCD'),
     update: function(str){
-        this.display.innerText=str;
-        this.display.setAttribute('aria-label', str);
+        this.display.innerText=str;        
     }
 }
